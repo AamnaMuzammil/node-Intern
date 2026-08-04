7ff27e3494f89408829a1f081d761ce7a439ce1b
Moiz Jawad <123112403+Moiz-jawad@users.noreply.github.com>
2026-08-04 16:50:30 +0500

    first commit

---
+++ variable.js
@@ -0,0 +1,30 @@
+console.log(x);
+var x = 5;
+console.log(x);
+
+var y = 10;
+console.log(y+x);
+
+var z = y * x;
+console.log(z);
+
+var a = 20;
+console.log(a);
+
+var b = a / 2;
+console.log(b);
+
+var c = b - x;
+console.log(c);
+
+var d = c % y;
+console.log(d);
+
+var name = "John";
+console.log(name);
+
+var greeting = "Hello, " + name + "!";
+console.log(greeting);
+
+var marks= [85, 90, 78, 92, 88];
+console.log(marks);
---
+++ variable_let.js
@@ -0,0 +1,55 @@
+// console.log(x);
+let x = 5;
+console.log(x);
+
+let y = 10;
+console.log(y+x);
+
+let z = y * x;
+console.log(z);
+
+let a = 20;
+console.log(a);
+
+let b = a / 2;
+console.log(b);
+
+let c = b - x;
+console.log(c);
+
+let d = c % y;
+console.log(d);
+
+let name = "John";
+console.log(name);
+
+let greeting = "Hello, " + name + "!";
+console.log(greeting);
+
+let marks= [85, 90, 78, 92, 88];
+console.log(marks);
+
+const pi = 3.14;
+console.log(pi);
+
+// var pi = 3.14;
+// console.log(pi);
+
+let radius = 15;
+let area = pi * radius * radius;
+console.log(area);
+
+// let radius = 2;
+// let area = pi * radius * radius;
+// console.log(area);
+
+// console.log(age);
+// const age = 25;
+// console.log(age);
+
+// let age = 25;
+// console.log(age);
+// let age = 30; 
+// console.log(age);
+ 
+
