0fe24f833272c892451d26c200d03cbc4a522e1c
Moiz Jawad <123112403+Moiz-jawad@users.noreply.github.com>
2026-08-05 14:00:31 +0500

    third

---
+++ test.js
@@ -0,0 +1,14 @@
+let a=10;
+let b=20;
+b=b-a
+a=a+b
+console.log("value of a is: ",a);
+console.log("value of b is: ",b);
+
+let c=10;
+let d=20;
+let e=c;
+c=d;
+d=e;
+console.log("value of c is: ",c);
+console.log("value of d is: ",d);
