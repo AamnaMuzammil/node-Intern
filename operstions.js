012b2fd6e312ca6ce7a5bb43fd3bdbe9a1db5be8
Moiz Jawad <123112403+Moiz-jawad@users.noreply.github.com>
2026-08-05 11:36:49 +0500

    Your descriptive commit message

---
+++ operstions.js
@@ -0,0 +1,75 @@
+var marks;
+marks=98;
+if(marks>=90){
+    console.log("A+");
+}
+else if(marks>=80){
+    console.log("A");
+}
+else if(marks>=70){
+    console.log("B+");
+}
+else if(marks>=60){
+    console.log("B");
+}
+else if(marks>=50){
+    console.log("C");
+}
+else{
+    console.log("Fail");
+}
+
+a=20;
+b=10;
+if (a>b){
+    console.log("A is greater than B");
+}
+else if(a<b){
+    console.log("A is LESS than B");
+}
+else if(a==b){
+    console.log("A is equal to B");
+}
+else if(a!=b){
+    console.log("A is not equal to B");
+}
+else if(a>=b){
+    console.log("A is greater than or equal to B");
+}
+else if(a<=b){
+    console.log("A is less than or equal to B");
+}
+else{
+    console.log("B is greater than A");
+}
+console.log("addition of a and b is: ",a+b);
+console.log("subtraction of a and b is: ",a-b);
+console.log("multiplication of a and b is: ",a*b);
+console.log("division of a and b is: ",a/b);
+console.log("modulus of a and b is: ",a%b);
+
+a=10;
+b=20;
+c=30;
+if(a>b&&a>c){
+    console.log("A is greater than B and C");
+}
+else if(b>a&&b>c){
+    console.log("B is greater than A and C");
+}
+else if(c>a&&c>b){
+    console.log("C is greater than A and B");
+}
+else{
+    console.log("All are equal");
+}
+
+var name="amna";
+if(name=="amna"){
+    console.log("Hello amna");
+}
+else if(name=="ali"){
+    console.log("Hello ali");}
+
+name="1234";
+number=1234;
